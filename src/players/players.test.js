537f3abf08720b3fc023bc6af3c4558086639e2b
Vinod Kumar Reddy Peram <vinodkumarreddyperam@Vinods-MacBook-Air.local>
2021-04-06 20:44:25 -0500

    Added test case for Player Symbol radio button selector

--- src/players/players.test.js
+++ src/players/players.test.js
@@ -9,7 +9,8 @@ describe('Players', () => {
 		playerName: 'Vinod',
 		currentSymbol: 'X',
 		alreadySelected: 'O',
-		changed: jest.fn()
+		changed: jest.fn(),
+		symbolHandler: jest.fn(),
 	}
 
 	it('Renders Players', () => {
@@ -25,6 +26,16 @@ describe('Players', () => {
 			wrapper.find('[data-testid="PlayerName"]').props().onChange();
 
 			expect(props.changed).toHaveBeenCalledTimes(1);
+		});
+	});
+
+	describe('When User selects Player\'s Symbol', () => {
+		it('Should Add Player\' Symbol in state memory', () => {
+			const wrapper = shallow(<Players {...props} />);
+
+			wrapper.find('[data-testid="SymbolX"]').props().onChange();
+
+			expect(props.symbolHandler).toHaveBeenCalledTimes(1);
 		});
 	});
 });
