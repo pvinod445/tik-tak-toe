93b5416744f8aeb421be70fb1997fd674957fb68
Vinod Kumar Reddy Peram <vinodkumarreddyperam@Vinods-MacBook-Air.local>
2021-04-06 21:19:45 -0500

    Test case for adding 2 player's to the game

--- src/gameFunctionality/gameFunctionality.test.js
+++ src/gameFunctionality/gameFunctionality.test.js
@@ -108,4 +108,75 @@ describe('GameFunctionality', () => {
 			expect(wrapper.state().players['X']).toEqual('Vinod');
 		});
 	});
+
+	describe('Trying to add all Player\'s Details', () => {
+		it('Should add all Players to the game', () => {
+			const wrapper = shallow(<GameFunctionality />);
+			let PlayersComponent = wrapper.find(Players);
+
+			let event = {
+				target: {
+					value: 'Vinod'
+				}
+			}
+
+			PlayersComponent.props().changed(event);
+
+			wrapper.update();
+			PlayersComponent = wrapper.find(Players);
+
+			expect(PlayersComponent.props().playerName).toEqual('Vinod');
+
+			event = {
+				target: {
+					value: 'X'
+				}
+			}
+
+			PlayersComponent.props().symbolHandler(event);
+
+			wrapper.update();
+			PlayersComponent = wrapper.find(Players);
+
+			expect(PlayersComponent.props().currentSymbol).toEqual('X');
+
+			PlayersComponent.props().clicked();
+
+			expect(wrapper.state().errors).toEqual(false);
+
+			expect(wrapper.state().players['X']).toEqual('Vinod');
+
+			event = {
+				target: {
+					value: 'Computer'
+				}
+			}
+
+			PlayersComponent.props().changed(event);
+
+			wrapper.update();
+			PlayersComponent = wrapper.find(Players);
+
+			expect(PlayersComponent.props().playerName).toEqual('Computer');
+
+			event = {
+				target: {
+					value: 'O'
+				}
+			}
+
+			PlayersComponent.props().symbolHandler(event);
+
+			wrapper.update();
+			PlayersComponent = wrapper.find(Players);
+
+			expect(PlayersComponent.props().currentSymbol).toEqual('O');
+
+			PlayersComponent.props().clicked();
+			wrapper.update();
+
+			expect(wrapper.state().players['X']).toEqual('Vinod');
+			expect(wrapper.state().players['O']).toEqual('Computer');
+		});
+	});
 });
